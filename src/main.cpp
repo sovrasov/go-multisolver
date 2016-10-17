c42f398b0b4dfbbbb663395c5fbcc7a6526b40e2
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2016-10-17 14:03:53 +0300

    Add draft of single-task mode

--- src/main.cpp
+++ src/main.cpp
@@ -17,43 +17,77 @@ int main(int argc, char** argv)
   parser.add<int>("threadsNum", 't', "number of threads in parallel method", false, 1,
      cmdline::range(1, 32));
   parser.add<double>("reliability", 'r', "reliability parameter for the method",
-    false, 3.5, cmdline::range(1., 1000.));
+    false, 4.5, cmdline::range(1., 1000.));
   parser.add<double>("accuracy", 'e', "accuracy of the method", false, 0.01);
   parser.add<int>("trialsLimit", 'l', "limit of trials for the method", false, 5000000);
   parser.add("saveStatistics", 's', "determines whether the method will "
     "save statistics of deviations");
   parser.add<std::string>("statFile", 'f', "name of the file to write statistics",
     false, "statistics.csv");
+  parser.add<std::string>("runMode", 'm', "",
+    false, "multi");
+  parser.add("hard", 'h', "determines type of GKLS functions");
   parser.parse_check(argc, argv);
 
-  const unsigned nProblems = 100;
-  ProblemsPool<gkls::GKLSFunction> pool;
-  for(unsigned i = 0; i < nProblems; i++)
-  {
-    gkls::GKLSFunction* func = new gkls::GKLSFunction();
-    func->SetFunctionClass(gkls::Simple, parser.get<int>("dimension"));
-    func->SetType(gkls::TD);
-    func->SetFunctionNumber(i + 1);
-    pool.Add(std::shared_ptr<gkls::GKLSFunction>(func));
-  }
-
-  std::cout << "Problems pool created\n";
-
+  gkls::GKLSClass problemsClass = parser.exist("hard") ? gkls::Hard : gkls::Simple;
   SolverParameters parameters(parser.get<double>("accuracy"),
-      parser.get<double>("reliability"),
-      parser.get<int>("threadsNum"),
-      parser.get<int>("trialsLimit"), StopType::OptimumVicinity);
+    parser.get<double>("reliability"),
+    parser.get<int>("threadsNum"),
+    parser.get<int>("trialsLimit"), StopType::OptimumVicinity);
   parameters.logDeviations = parser.exist("saveStatistics");
+  std::vector<StatPoint> statistics;
+  const unsigned nProblems = 100;
 
-  GOSolver<gkls::GKLSFunction> solver;
-  solver.SetParameters(parameters);
-  solver.SetProblemsPool(pool);
-  std::cout << "Solver started\n";
-  auto start = std::chrono::system_clock::now();
-  solver.Solve();
-  auto end = std::chrono::system_clock::now();
-  std::vector<Trial> optimumEstimations = solver.GetOptimumEstimations();
-  std::vector<StatPoint> statistics = solver.GetStatistics();
+  if (parser.get<std::string>("runMode") == std::string("multi"))
+  {
+    ProblemsPool<gkls::GKLSFunction> pool;
+    for (unsigned i = 0; i < nProblems; i++)
+    {
+      gkls::GKLSFunction* func = new gkls::GKLSFunction();
+      func->SetFunctionClass(problemsClass, parser.get<int>("dimension"));
+      func->SetType(gkls::TD);
+      func->SetFunctionNumber(i + 1);
+      pool.Add(std::shared_ptr<gkls::GKLSFunction>(func));
+    }
+
+    std::cout << "Problems pool created\n";
+
+    GOSolver<gkls::GKLSFunction> solver;
+    solver.SetParameters(parameters);
+    solver.SetProblemsPool(pool);
+    std::cout << "Solver started\n";
+    auto start = std::chrono::system_clock::now();
+    solver.Solve();
+    auto end = std::chrono::system_clock::now();
+    std::vector<Trial> optimumEstimations = solver.GetOptimumEstimations();
+    statistics = solver.GetStatistics();
+
+    std::cout << "Number of trials: " << solver.GetTrialsNumber()
+      << "\nNumber of iterations: " << solver.GetIterationsNumber() << "\n";
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "Time elapsed: " << elapsed_seconds.count() << "s\n";
+    //  for(size_t i = 0; i < optimumEstimations.size(); i++)
+    //    std::cout << "Optimum value in problem #" << i + 1 << ": " << optimumEstimations[i].z << "\n";
+  }
+  else
+  {
+    for (unsigned i = 0; i < nProblems; i++)
+    {
+      ProblemsPool<gkls::GKLSFunction> pool;
+      gkls::GKLSFunction* func = new gkls::GKLSFunction();
+      func->SetFunctionClass(problemsClass, parser.get<int>("dimension"));
+      func->SetType(gkls::TD);
+      func->SetFunctionNumber(i + 1);
+      pool.Add(std::shared_ptr<gkls::GKLSFunction>(func));
+
+      GOSolver<gkls::GKLSFunction> solver;
+      solver.SetParameters(parameters);
+      solver.SetProblemsPool(pool);
+
+      //statistics.push_back();
+    }
+  }
 
   if(parameters.logDeviations)
   {
@@ -66,14 +100,7 @@ int main(int argc, char** argv)
 
    fout.close();
   }
-//  for(size_t i = 0; i < optimumEstimations.size(); i++)
-//    std::cout << "Optimum value in problem #" << i + 1 << ": " << optimumEstimations[i].z << "\n";
 
-  std::cout << "Number of trials: " << solver.GetTrialsNumber()
-    << "\nNumber of iterations: " << solver.GetIterationsNumber() << "\n";
-
-  std::chrono::duration<double> elapsed_seconds = end - start;
-  std::cout << "Time elapsed: " << elapsed_seconds.count() << "s\n";
 
   return 0;
 }
