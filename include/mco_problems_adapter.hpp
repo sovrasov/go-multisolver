ce90dd7fbf402286c787ba2eee37367e9a530fed
sovrasov <sovrasov.vlad@gmail.com>
2019-11-26 13:23:48 +0300

    Add mco sample

---
+++ include/mco_problems_adapter.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+#include <memory>
+#include <algorithm>
+#include <exception>
+
+
+class MultiObjectiveProblemAdapter
+{
+public:
+  using FuncPtr = std::function<double(const double*)>;
+protected:
+  std::vector<FuncPtr> mObjectives;
+  std::vector<FuncPtr> mConstraints;
+  std::vector<double> mLBound;
+  std::vector<double> mUBound;
+  std::vector<double> mLambdas;
+  size_t mNumberOfParetoPoints;
+
+public:
+  MultiObjectiveProblemAdapter() : mNumberOfParetoPoints(0) {}
+  MultiObjectiveProblemAdapter(const std::vector<FuncPtr>& objectives,
+                        const std::vector<FuncPtr>& constraints,
+                        const std::vector<double>& lb, const std::vector<double>& ub,
+                        size_t numParetoPoints = 100) :
+                        mObjectives(objectives), mConstraints(constraints),
+                        mLBound(lb), mUBound(ub)
+  {
+    if (mLBound.size() != mUBound.size() || mLBound.size() < 1)
+      throw std::runtime_error("Bounds dimension is incorrect");
+    if (numParetoPoints < 1)
+      throw std::runtime_error("There sould be more than one Pareto point");
+    mNumberOfParetoPoints = numParetoPoints;
+    double h = 1. / (mNumberOfParetoPoints - 1);
+    for (size_t i = 0; i < mNumberOfParetoPoints; i++)
+      mLambdas.push_back(i*h);
+  }
+
+  void GetBounds(double* lb, double* ub, unsigned problemIndex)
+  {
+    std::copy_n(mLBound.begin(), mLBound.size(), lb);
+    std::copy_n(mUBound.begin(), mUBound.size(), ub);
+  }
+
+  unsigned GetConstraintsNumber(unsigned problemIndex) const
+  {
+    return mConstraints.size();
+  }
+
+  unsigned GetDimension(unsigned problemIndex = 0) const
+  {
+    return mUBound.size();
+  }
+
+  unsigned GetSize() const
+  {
+    return mNumberOfParetoPoints;
+  }
+
+  double CalculateObjective(const double* y, unsigned problemIdx, unsigned fIndex=0)
+  {
+    if (fIndex == GetConstraintsNumber(problemIdx))
+      return fmax(mObjectives[0](y) * mLambdas[problemIdx], mObjectives[1](y) * (1. - mLambdas[problemIdx]));
+    else
+      return mConstraints[fIndex](y);
+  }
+
+  double GetOptimalValue(unsigned problemIndex) const
+  {
+    return 0;
+  }
+
+  void GetOptimumCoordinates(double* y, unsigned problemIndex) const
+  {
+  }
+};
---
+++ src/solve_mco_sample.cpp
@@ -0,0 +1,59 @@
+#include <json.hpp>
+#include <cmdline.h>
+
+#include <iostream>
+
+#include "mco_problems_adapter.hpp"
+#include "goSolver.hpp"
+
+int main(int argc, char** argv)
+{
+  cmdline::parser parser;
+  parser.add<int>("threadsNum", 'p', "number of threads in parallel method", false, 1,
+     cmdline::range(1, 32));
+  parser.add<int>("evolventTightness", 't', "", false, 12,
+        cmdline::range(9, 16));
+  parser.add<double>("reliability", 'r', "reliability parameter for the method",
+    false, 4.5, cmdline::range(1., 1000.));
+  parser.add<double>("accuracy", 'e', "accuracy of the method", false, 0.01);
+  parser.add<int>("trialsLimit", 'l', "limit of trials for the method", false, 5000000);
+  parser.add<unsigned>("problemsNum", 'n', "number of pareto points", false, 100);
+  parser.add("saveStatistics", 's', "determines whether the method will "
+    "save statistics of deviations");
+  parser.add("verbose", 'v', "determines whether the method print information messages");
+  parser.add<std::string>("statFile", 'f', "name of the file to write statistics",
+    false, "statistics.csv");
+  parser.add<std::string>("runMode", 'm', "", false, "multi", cmdline::oneof(
+    std::string("multi"), std::string("synch"), std::string("asynch")));
+  parser.parse_check(argc, argv);
+
+  auto f1 = [](const double* x) {return 4*(x[0]*x[0] + x[1]*x[1]);};
+  auto f2 = [](const double* x) {return pow(x[0] - 5, 2) + pow(x[1] - 5, 2);};
+  auto g1 = [](const double* x) {return pow(x[0] - 5, 2) + x[1] * x[1] - 25;};
+  auto g2 = [](const double* x) {return -pow(x[0] - 8, 2) - pow(x[1] + 3, 2) + 7.7;};
+
+  MultiObjectiveProblemAdapter problem({f1, f2}, {g1, g2}, {0, 0}, {5, 3}, 100);
+
+  GOSolver<MultiObjectiveProblemAdapter> solver;
+  SolverParameters parameters(parser.get<double>("accuracy"),
+    parser.get<double>("reliability"),
+    parser.get<int>("threadsNum"),
+    parser.get<int>("trialsLimit"), StopType::Accuracy);
+  parameters.verbose = parser.exist("verbose");
+  parameters.evloventTightness = parser.get<int>("evolventTightness");
+  solver.SetParameters(parameters);
+  solver.SetProblemsPool(problem);
+  std::cout << "Solver started\n";
+  solver.Solve();
+  std::vector<Trial> optimumEstimations = solver.GetOptimumEstimations();
+  for (const auto& paretoPoint : optimumEstimations)
+  {
+    double f1_val = f1(paretoPoint.y);
+    double f2_val = f2(paretoPoint.y);
+    if (paretoPoint.v == 2)
+      std::cout << "Pareto point: " << f1_val << ", " << f2_val << "\n";
+    else
+      std::cout << "Failed to find a feasible point!" << '\n';
+  }
+  return 0;
+}
