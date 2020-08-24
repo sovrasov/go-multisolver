c0ac6d8d72c0b4d9b6b79217ae15761cbea45686
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2020-08-24 12:34:43 +0300

    Update 4d exps

--- experiments/4d_constr/run.sh
+++ experiments/4d_constr/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a threads=("1" "2" "4" "6")
-declare -a delays=("0.5" "1")
+declare -a delays=("1")
 declare -a modes=("multi" "asynch")
 executable='../bin/solve_constrained_sample'
 
@@ -15,7 +15,7 @@ do
     delay_str=${delay//[.]/}
     for t_num in "${threads[@]}"
     do
-      command="$executable -n 20 -p $t_num -m $mode -d $dim -e 0.02 -r 4.7 -s --delay $delay -f gkls_"$dim"d_m_"$mode_str"_p_"$t_num"_d_$delay_str.csv"
+      command="$executable -n 20 -l 17000000 -p $t_num -m $mode -d $dim -e 0.02 -r 4.7 -s --delay $delay -f gkls_"$dim"d_m_"$mode_str"_p_"$t_num"_d_$delay_str.csv"
       echo $command
       $command
     done
